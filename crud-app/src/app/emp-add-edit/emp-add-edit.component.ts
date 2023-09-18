78ee01ead24cd5da7d6b461e85fe5b1c372a7e66
nguyenhuutam-vt <nguyenvinhspkt10@gmail.com>
2023-09-18 07:14:40 +0700

    update

--- crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
+++ crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss'],
 })
 export class EmpAddEditComponent {
+  empForm: FormGroup;
+
   education: string[] = ['Matric', 'Intermedia', 'Diploma', 'Graduate'];
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    });
+  }
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      console.log(this.empForm.value);
+    }
+  }
 }
